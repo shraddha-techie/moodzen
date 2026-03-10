a0e63fbc07d1bf036fb86e0169321ffc48d94bc8
shraddha-techie <shraddhapersonal2003@gmail.com>
2026-03-10 06:52:56 +0530

    update moodzen

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from database import save_songs, get_songs_from_db
 
 st.set_page_config(page_title="MoodZen 3.0.0", page_icon="🎵")
 
-st.title("🎵 MoodZenn 3.0")
+st.title("🎵 MoodZenn 2.0")
 st.success("✅ MoodZen CI/CD deployment test running")
 st.write("Select your mood and get fresh songs from the Internet 🎧")
 
 mood = st.radio(
     "tell your mood",
-    ["Happy", "Sad", "Relaxed", "Energetic","motivation"]
+    ["Happy", "Sad", "Relaxed", "Energetic"]
 )
 
 if mood:
